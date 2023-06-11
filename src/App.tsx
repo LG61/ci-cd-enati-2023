69c99e23ca33fbd6d1c0c9aac096d77a401f92b3
Thiago Cesar <thiagocesarmata@gmail.com>
2023-06-10 22:27:16 -0300

    setup initial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> ENATI 2023 </h1>
+        <h1> Introdução ao CI/CD </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
